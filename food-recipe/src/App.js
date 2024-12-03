8864fe833c5098c598db5d0ce9507ae436eddc49
YAZILI\sena.canik <sena.canik@sabah.com.tr>
2024-12-03 09:38:10 +0300

    feat: add React setup

---
+++ food-recipe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React'e Geçiş Başarılı 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
